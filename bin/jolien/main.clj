27cf1f8b069260a86ff17c57e7e4a0631189b64c
jodecler <declerckjolien@gmail.com>
2017-05-02 11:21:08 +0200

    connection github + classification

--- bin/jolien/main.clj
+++ bin/jolien/main.clj
@@ -1,7 +1,7 @@
 (ns ast.main
   (:require [jolien.classification :as classification])
-  (:require [jolien.toplevelcategorize :as categorize]))
-
+  (:require [jolien.toplevelcategorize :as categorize])
+  (:require [qwalkeko.experiments.automated :as automated]))
 
 (defn algemene-loop [first-file second-file]
   (let [categorie (categorize/top-level first-file)]
@@ -10,9 +10,6 @@
             (= categorie 'source-layer))
         (classification/differences-two-files first-file second-file)
         )))
-
-
-
 
 (defn try [] 
   (algemene-loop
@@ -52,5 +49,34 @@
    "/Users/joliendeclerck/Documents/workspace/damp.qwalkeko.plugin/src/jolien/files/Source.java"
    "/Users/joliendeclerck/Documents/workspace/damp.qwalkeko.plugin/src/jolien/files/Source.java"))  
 
-      
 
+(defn test []
+  (automated/do-import))
+
+(defn get-changes-out-of-file []
+  (automated/clone-projects ast.main/PROJECT_FOLDER ast.main/PROJECTS)
+  (automated/import-projects ast.main/PROJECT_FOLDER))
+  (let [information (automated/find-all-changes automated/BREAKERFIXER)]
+    (doseq [i information]
+      (let [changed-files (:changed i)
+            changes (:changes i)]
+        (doseq [file changed-files]
+          (let [categorie (categorize/top-level file)]
+            (categorize/pretty-print-type-file file categorie)
+            (if (or (= categorie 'test-layer)
+                    (= categorie 'source-layer))
+              (classification/classify-changes changes))))
+            ))
+        
+    ))
+
+  
+;  {
+;     :project (nth change 0),
+;     :breaking (nth change 1),
+;     :fixing (nth change 2),
+;     :breaking_tr (nth change 3),
+;     :fixing_tr (nth change 4),
+;     :changed (first changes),
+;     :changes (second changes)
+;    }
